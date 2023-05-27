986085daa6a2e553262e623d631ad595e5a83a1c
Linchaker <linchaker@gmail.com>
2023-05-27 19:49:53 +0300

    auth front

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const ordersRoutes = require('./routes/orders')
 const coursesRoutes = require('./routes/courses')
+const authRoutes = require('./routes/auth')
 const User = require('./models/user')
+
 
 const app = express()
 
@@ -45,6 +47,7 @@ app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 app.use('/orders', ordersRoutes)
+app.use('/auth', authRoutes)
 
 
 
